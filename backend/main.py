e23d609b9729813bd361f2a3f6eb5f51fb3097e9
NataliVins <natalimzxcv@gmail.com>
2024-05-01 12:24:21 +0200

    docker container for client was added

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Убрали PUT и DELETE из списка
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
 
 @app.get('/')
 async def root():
@@ -11,4 +22,3 @@ async def root():
 @app.post('/')
 async def add_new_breed(body: dict):
     return {"body": body}
-
