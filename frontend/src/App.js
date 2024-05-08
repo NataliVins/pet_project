31f265b6621e2c916b487902fdf8f357b52adb01
NataliVins <natalimzxcv@gmail.com>
2024-05-08 08:50:07 +0200

    update html code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,43 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [dogName, setDogName] = useState('');
+
+  const handleInputChange = (event) => {
+    setDogName(event.target.value);
+  };
+
+  const sendData = () => {
+    fetch('http://localhost:8000', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ key: 'value' }),
+}).then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test777754343
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>dog breed</h1>
+        <div className="photo-cell">
+          <img src="media/doberman.jpg" alt="Dog Photo" />
+          <div className="button-container">
+            <button className="button">Add info</button>
+            <button className="button">Delete</button>
+          </div>
+        </div>
+        <div>
+          <h1>Input your dog name:</h1>
+          <input type="text" value={dogName} onChange={handleInputChange} name="dogname" />
+          <button onClick={sendData}>Send</button>
+        </div>
       </header>
     </div>
   );
